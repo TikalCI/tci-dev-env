588389ef9f7c44ff2a82712df870bc4328ecae16
yorammi <yorammi@tikalk.com>
2019-01-01 10:00:19 +0200

    checkout branch for module [Branch] master

--- init-submodule.sh
+++ init-submodule.sh
@@ -4,6 +4,11 @@ set -e
 
 if [[ $# > 0 ]]; then
    submodule=$1
+    if [[ $# > 1 ]]; then
+       branch=$2
+    else
+       branch=master
+    fi
 else
    echo "You must specify the submodule name as a parameter"
    exit 1
@@ -11,6 +16,7 @@ fi
 
 git submodule update --init $submodule
 cd $submodule
+git checkout $branch | true
 git status
 cd ..
 ./tci-dev-env.sh info
